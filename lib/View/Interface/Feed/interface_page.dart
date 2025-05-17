c71bb41e11c04dc83f5d41fa9357e9282eaa71e0
Moiz <khanmoaiz682@gmail.com>
2025-05-17 22:02:48 +0500

    Remove unused Iconsax import from interface_page.dart to streamline dependencies and improve code clarity.

--- lib/View/Interface/Feed/interface_page.dart
+++ lib/View/Interface/Feed/interface_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:iconsax/iconsax.dart';
 import 'package:provider/provider.dart';
 import 'package:social_media/Controller/Services/Database/database_services.dart';
 import 'package:social_media/Utils/Components/post_card.dart';
